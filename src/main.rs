fde69614ae7bb9fa90da9ed1e52116b472740400
Seimizu Joukan <joukan.seimizu@gmail.com>
2026-09-12 19:51:14 +0900

    Report DTB-discovered GICv2 interfaces

--- src/main.rs
+++ src/main.rs
@@ -145,6 +145,10 @@ extern "C" fn rust_main(argc: isize, argv: *const *const u8) -> ! {
             halt();
         }
     };
+    println!(
+        "GICv2 DTB discovery: GICD={} GICC={} GICH={} GICV={}",
+        gic.distributor, gic.cpu_interface, gic.hypervisor_interface, gic.virtual_cpu_interface,
+    );
     gicv2::initialize(gic);
 
     // Set up bootstrap page table
